5981f6ddff9490e85e1d174877c68351d4ad7623
Dusan Miloradovic <dusan@gasco.ae>
2018-12-06 09:19:33 +0400

    remove inner context function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 const RootContext = React.createContext({
   addInnerContext: contextId => {},
   getInnerContext: contextId => {},
+  removeInnerContext: contextId => {},
   setInnerState: (contextId, state) => {},
   getInnerState: contextId => {}
 });
@@ -14,6 +15,8 @@ export default class extends React.Component {
     this.getInnerContext = this.getInnerContext.bind(this);
     this.setInnerState = this.setInnerState.bind(this);
     this.getInnerState = this.getInnerState.bind(this);
+    this.removeInnerContext = this.removeInnerContext.bind(this);
+
     this.state = {
       innerContexts: {},
       root: {
@@ -38,6 +41,15 @@ export default class extends React.Component {
       return { innerContexts: st };
     });
     return ctx;
+  }
+
+  removeInnerContext(contextId) {
+    this.setState((state, props) => {
+      if (!state.innetContexts[contextId]) return null;
+      let st = { ...state.innerContexts };
+      delete st[contextId];
+      return { innerContexts: st };
+    });
   }
 
   getInnerContext(contextId) {
